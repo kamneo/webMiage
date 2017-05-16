21988bdec94fcd6d8beb80bbc4849cdd4e158215
btoulouse <btoulouse@dama.emi.u-bordeaux.fr>
2017-05-16 16:41:13 +0200

    Ajout des commentaires

--- src/com/model/Escalier.java
+++ src/com/model/Escalier.java
@@ -9,6 +9,12 @@ public class Escalier {
 	private String nom;
 	private HashMap<Integer,Rang> rang;
 	
+	/**
+	 * Constructeur
+	 * @param nom 		= nom de l'escalier
+	 * @param escaliers = Liste des rangs qui compose l'escalier
+	 * Construit l'objet Escalier du nom de @param nom et crée HashMap avec des couples id/escalier
+	 */
 	public Escalier(String nom, Rang[] rangs) {
 		this.nom=nom;
 		this.rang=new HashMap<>();
@@ -17,16 +23,31 @@ public class Escalier {
 		}
 	}
 	
+	/**
+	 * getNom renvoi le nom de l'escaliler
+	 */
 	public String getNom() {
 		return nom;
 	}
+	
+	/**
+	 * setNom permet de changer le nom de l'escalier
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-	public HashMap<Integer, Rang> getEscalier() {
+	
+	/**
+	 * getRang renvoi la liste des rang et de leur id
+	 */
+	public HashMap<Integer, Rang> getRang() {
 		return rang;
 	}
-	public void setEscalier(HashMap<Integer, Rang> rangs) {
+	
+	/**
+	 * setRang permet de remplacer la liste des couples id/rang pour une autre liste de couple
+	 */
+	public void setRang(HashMap<Integer, Rang> rangs) {
 		this.rang = rangs;
 	}
 }
--- src/com/model/Orientation.java
+++ src/com/model/Orientation.java
@@ -7,6 +7,12 @@ public class Orientation {
 	private String nom;
 	private HashMap<Integer,Escalier> escalier;
 	
+	/**
+	 * Constructeur
+	 * @param nom 		= nom de l'oriantation : EST, OUEST, NORD, SUD
+	 * @param escaliers = Liste des escaliers qui compose l'orientation
+	 * Construit l'objet Oriantation du nom de @param nom et crée HashMap avec des couples id/Escalier
+	 */
 	public Orientation(String nom, Escalier[] escaliers) {
 		this.nom=nom;
 		this.escalier=new HashMap<>();
@@ -15,15 +21,30 @@ public class Orientation {
 		}
 	}
 	
+	/**
+	 * getNom renvoi le nom de l'orientation
+	 */
 	public String getNom() {
 		return nom;
 	}
+	
+	/**
+	 * setNom permet de changer le nom de l'orientation
+	 */
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+	
+	/**
+	 * getEscalier renvoi la liste des Escaliers et de leur id
+	 */
 	public HashMap<Integer, Escalier> getEscalier() {
 		return escalier;
 	}
+	
+	/**
+	 * setNom permet de remplacer la liste des couples id/Escalier pour une autre liste de couple
+	 */
 	public void setEscalier(HashMap<Integer, Escalier> escalier) {
 		this.escalier = escalier;
 	}
