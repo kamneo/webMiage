29b6ccb75fb3d79522b5ead7168ab183c1c8f9c8
jbazerque <jbazerque@bellonda.emi.u-bordeaux.fr>
2017-05-16 14:36:26 +0200

    coucou

--- src/com/model/Place.java
+++ src/com/model/Place.java
@@ -3,6 +3,26 @@ package com.model;
 public class Place {
 	private static int numero;
 	private static boolean estAchetee;
-	
-	
+
+	public Place(int numero, boolean estAchetee) {
+		this.setNumero(numero);
+		this.setEstAchetee(false);
+	}
+
+	public static int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		Place.numero = numero;
+	}
+
+	public static boolean isEstAchetee() {
+		return estAchetee;
+	}
+
+	public void setEstAchetee(boolean estAchetee) {
+		Place.estAchetee = estAchetee;
+	}
+
 }
