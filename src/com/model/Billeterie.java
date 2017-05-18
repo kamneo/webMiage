e6c9ea46ec71b10bb93600acdc3c3ee4503a20d1
btoulouse <btoulouse@lilienthal.emi.u-bordeaux.fr>
2017-05-18 16:02:30 +0200

    rebase

--- src/com/model/Billeterie.java
+++ src/com/model/Billeterie.java
@@ -9,22 +9,37 @@ public class Billeterie {
 	private HashMap<Integer, Evenement> evenements;
 	private HashMap<Integer, Double> reduction;
 	
-	
+	/**
+	 * Constructeur par défaut
+	 * Crée une billéterie avec un liste vide 
+	 */
 	public Billeterie(){
 		setStade(new Stade_Impl());
 		evenements = new HashMap<Integer, Evenement>();
 	}
 	
-	
-	public double acheterPlace(int idEvent, String idCat, int idReduction){
-		double prixPlace = evenements.get((Integer)idEvent).getPrixCat(idCat) * reduction.get((Integer)idReduction);
+	/**
+	 * @param idEvent - id de l'event concerné
+	 * @param idCat - id de la catégorie concerné
+	 * @param idReduction - id de la 
+	 * @return le prix de la prix d'une catégorie pour un evenement donné et une réduction donnée
+	 */
+	public double CalculerPrix(int idEvent, String idCat, String categoriePersonne){
+		double prixPlace = evenements.get(idEvent).getPrixCat(idCat) * reduction.get(categoriePersonne);
 		return prixPlace;
 	}
 
+	/**
+	 * @return stade attacher à la Billeterie
+	 */
 	public Stade_Impl getStade() {
 		return stade;
 	}
 
+	/**
+	 * @param stade - nouveau stade à lier a la Billeterie
+	 * permet de changer le stade lié à la Billeterie
+	 */
 	public void setStade(Stade_Impl stade) {
 		this.stade = stade;
 	}
