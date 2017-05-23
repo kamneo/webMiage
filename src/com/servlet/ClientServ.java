86e00856a5c7cca499f70e1c8c7b29b8be32fc09
btoulouse <btoulouse@lutetia.emi.u-bordeaux1.fr>
2017-05-23 11:59:33 +0200

    zdfzf

--- src/com/servlet/ClientServ.java
+++ src/com/servlet/ClientServ.java
@@ -5,12 +5,16 @@ import java.text.ParseException;
 import java.util.HashMap;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.model.Client;
-
+/**
+ * Servlet implementation class Stade
+ */
+@WebServlet("/ClientServ")
 public class ClientServ extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String mail = request.getParameter("mail");
