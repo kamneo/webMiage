fd1df5a197e5cf9f3de9f349db64359fe8b1bf02
kamneo <alexandre.pinero@gmail.com>
2017-06-07 00:10:49 +0200

    ajout du chargement du context au lancement et à la fermeture du serveur

---
+++ src/com/servlet/InitializeListner.java
@@ -0,0 +1,30 @@
+package com.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.xml.bind.JAXBException;
+
+import com.model.Billeterie;
+import com.serialisation.XMLMarshall;
+
+@WebListener
+public class InitializeListner implements ServletContextListener {
+
+	@Override
+	public final void contextInitialized(final ServletContextEvent sce) {
+		try {
+			Billeterie b = Billeterie.getInstance(); 
+			b.load((Billeterie)XMLMarshall.XMLTo(Billeterie.class, sce.getServletContext().getRealPath("/DataBase/billetterie.xml")));
+		} catch (JAXBException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public final void contextDestroyed(final ServletContextEvent sce) {
+		Billeterie b = Billeterie.getInstance(); 
+		XMLMarshall.ObjectToXML(b, sce.getServletContext().getRealPath("/DataBase/billetterie.xml"));
+	}
+}
