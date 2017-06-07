1eaa1be0d38ba9586d825ed67070486e1cd3abf6
kamneo <alexandre.pinero@gmail.com>
2017-06-07 22:55:14 +0200

    mise en place du mapping pour l'api

--- src/com/serialisation/XMLMarshall.java
+++ src/com/serialisation/XMLMarshall.java
@@ -1,6 +1,7 @@
 package com.serialisation;
 
 import java.io.File;
+import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
@@ -33,5 +34,18 @@ public class XMLMarshall {
 
 	    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 	    return jaxbUnmarshaller.unmarshal(file);
+	}
+	
+	public static String ObjectToString(Object o) throws JAXBException {
+		java.io.StringWriter sw = new StringWriter();
+
+	    JAXBContext jaxbContext = JAXBContext.newInstance(o.getClass());
+		Marshaller marshaller = jaxbContext.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		marshaller.marshal(o, sw);
+		
+		return sw.toString();
+
 	}
 }
