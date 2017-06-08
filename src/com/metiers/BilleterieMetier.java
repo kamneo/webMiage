caaf1d000ec2c680005b6c0da0b906bb3f83cbd6
kamneo <alexandre.pinero@gmail.com>
2017-06-09 01:02:10 +0200

    creation de la fonction de reservation

--- src/com/metiers/BilleterieMetier.java
+++ src/com/metiers/BilleterieMetier.java
@@ -4,63 +4,75 @@ import java.text.ParseException;
 import java.util.HashMap;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import com.model.Billet;
 import com.model.Billeterie;
+import com.model.Categorie;
+import com.model.Escalier;
+import com.model.Orientation;
+import com.model.Place;
+import com.model.Rang;
 import com.model.Reservation;
+import com.model.Evenement.Evenement;
 
 @Path("/billetterie")
 @Produces("application/xml")
 public class BilleterieMetier {
 	private static Billeterie billeterie;
+
 	/**
 	 * Methode qui permet d'instancier la billeterie pour un evenement
 	 */
-	public BilleterieMetier(){
+	public BilleterieMetier() {
 		billeterie = Billeterie.getInstance();
 	}
-/**
- * Methode pour creer un evenment sportif
- * @param nomEv Nom de l'evenement
- * @param date Date de l'evenement
- * @param tarif Objet de type Map contenant les different tarifs applicables
- * @param equipe1 Nom de l'equipe domicile
- * @param equipe2 Nom de l'equipe exterieure
- * @param description Commentaires sur l'evenement
- * @throws ParseException 
- */
-	public static void creerSport(String nomEv, String date, HashMap<String, Double> tarif, String equipe1, String equipe2, String description) throws ParseException {
-		billeterie.creerSport(nomEv, date, tarif, equipe1, equipe2, description);
-	}
-/**
- * Methode pour creer un evenement musical
- * @param nomEv Nom de l'evenement
- * @param date Date de l'evenement
- * @param tarif Objet de type Map contenant les different tarifs applicables
- * @param description Commentaires sur l'evenement
- * @throws ParseException
- */
-	public static void creerMusique(String nomEv,  String date, HashMap<String, Double> tarif, String description) throws ParseException {
-		billeterie.creerMusique(nomEv,  date, tarif, description);
-	}
+
 	/**
-	 * Methode pour supprimer un evenement
-	 * @param nomEv Nom de l'evenement
-	 */
-	public static void supprimerEvenement(String nomEv) {
-		billeterie.supprimerEvenement(nomEv);
-	}
-	/**
-	 * Methode pour valider une reservation de une ou plusieurs places
-	 * @param res Objet tableau contenant les places a reserver
+	 * Methode pour valider une reservation d'une place
+	 * 
+	 * @param idEv identifiant de l'evenement
+	 * @param nomOr nom de l'orientation souhaité
+	 * @param nomEsc nom de l'escalier
+	 * @param numRang numéro de rang
+	 * @param numPlace numéro de place
+	 * @param idReduc type de réduction ({(1 pour 30% de reduc), (2 pour 10%), (3 pour plien tarif)}
+	 * @param mail email de l'acheteur
 	 * @throws Exception
 	 */
-	public static void validerReservation(Reservation[] res) throws Exception{
-		for(int i=0;i<res.length;i++){ 
-			billeterie.acheterPlace(res[i]);
-		}
+	@GET
+	@Path("reserver/{idEv}/{nomOr}/{nomEsc}/{numRang}/{numPlace}/{idReduc}/{mail}")
+	public static void reserver(@PathParam("idEv") String idEv, @PathParam("nomOr") String nomOr,
+			@PathParam("nomEsc") String nomEsc, @PathParam("numRang") String numRang, @PathParam("numPlace") String numPlace,
+			@PathParam("idReduc") String idReduc, @PathParam("mail") String mail) throws Exception {
+		Billeterie b = Billeterie.getInstance();
+		Evenement e = b.getEvenements().get(Integer.parseInt(idEv));
+		Orientation or = e.getStade().getOrientations().stream().filter(o -> o.getNom().equals(nomOr)).findFirst()
+				.get();
+
+		Escalier esc = or.getEscalier().stream().filter(es -> es.getNomEsc().equals(nomEsc)).findFirst().get();
+
+		Rang rang = esc.getRang().stream().filter(r -> r.getNumeroRang() == Integer.parseInt(numRang)).findFirst()
+				.get();
+
+		Place pl = rang.getPlaces().stream().filter(p -> p.getNumero() == Integer.parseInt(numPlace)).findFirst().get();
+
+		String idCat = "categorie 1";
+		for (Categorie c : b.getCategoriesDisponibles())
+			if(!c.getNomCat().equals("OR"))
+				for (String s : c.getEscalier())
+					if (s.equals(esc.getNomEsc()))
+						idCat = c.getNomCat();
+
+		double prix = b.getReduction().get(Integer.parseInt(idReduc))
+				* (or.getNom().equals("Fosse") ? e.getPrixCat("OR") : e.getPrixCat(idCat));
+
+		billeterie.acheterPlace(new Reservation(e.getNomEv(), new Billet(or, esc, rang, pl, prix), mail));
 	}
+
 }
